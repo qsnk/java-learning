152a01576436c60aaf744535d7f7732c326be1f3
qsnka <mikhailroot01@gmail.com>
2026-07-29 01:01:38 +0300

    1.10 - Исключения + Дополнение к 2.1

---
+++ 1_basics/1_10_exceptions/Program.java
@@ -0,0 +1,77 @@
+public class Program {
+    public static void main(String[] args) {
+        // Throwable - базовый класс для всех исключений
+
+        int[] numbers = new int[] {1, 2, 3, 4, 5};        
+        System.out.printf("Первый элемент массива: %d\n", numbers[0]);
+
+        // Попытка достать 999 элемент
+        try {
+            int index = 999;
+            System.out.printf("%d элемент массива: %d\n", index, numbers[index]);        
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.printf("Ошибка при попытке получить элемент массива: %s\n", e.getMessage());
+        }
+        
+        // Попытка достать 999 элемент или последний
+        try {
+            int index = -1;
+            System.out.printf("%d элемент массива: %d\n", index, numbers[index]);        
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.printf("Ошибка при попытке получить элемент массива: %s\n", e.getMessage());
+        } finally {
+            System.out.println("Дошли до блока finally, пробуем получить последний элемент массива");
+            System.out.printf("Последний элемент массива: %d\n", numbers[numbers.length - 1]);        
+        }
+
+        // Бросаем исключение
+        int index = 100;
+        try {
+            int value = getArrayItem(numbers, index);
+            System.out.printf("%d элемент массива = %d\n", index, value);
+        } catch (Exception e) {
+            System.err.printf("Не удалось получить %d элемент массива: %s\n", index, e.getMessage());
+        }
+
+        // Использование своего исключения
+        try {
+            String value = "Java";
+            scanString(value);
+        } catch (Exception e) { // Ловим все исключения
+            System.err.printf("Ошибка при сканировании строки: %s\n", e.getMessage());
+        }
+
+        double avgValue = avg(numbers);
+        System.out.printf("Среднее арифметическое переданного массива = %f\n", avgValue);
+    }
+
+    // Функция, которая возвращает элемент массива и выбрасывает исключение ArrayIndexOutOfBoundsException в случае ошибки 
+    public static int getArrayItem(int[] array, int index) throws ArrayIndexOutOfBoundsException {
+        if (index >= array.length || index < 0) throw new ArrayIndexOutOfBoundsException(String.format("Index %d out of bounds for array with length %d", index, array.length));
+        return array[index];
+    }
+
+    // Функция, которая проверяет длину строки и выбрасывает исключение TooShortStringException в случае если строка короче 5 символов
+    public static void scanString (String value) throws TooShortStringException {
+        if (value.length() < 5) throw new TooShortStringException(String.format("String \"%s\" has too short length = %d", value, value.length()));
+        System.out.printf("%s has suitable length %d\n", value, value.length());
+    }
+
+    // Функция с оператором assert
+    public static double avg(int[] array) {
+        assert array.length > 0;
+
+        int sum = 0;
+
+        for (int i : array) {
+            sum += i;
+        }
+        return sum / array.length;
+    }
+}
+
+class TooShortStringException extends Exception {
+    public TooShortStringException(String message) {
+        super(message);
+    }
+}
--- 2_oop/2_1_classes/Program.java
+++ 2_oop/2_1_classes/Program.java
@@ -5,12 +5,12 @@ public class Program {
 
         list.insert("Node1");
         list.display();
-        
+
         list.insert("Node2");
-        list.display(); 
-        
+        list.display();
+
         list.insert("Node3");
-        list.display();        
+        list.display();
 
         list.remove(2);
         list.display();
@@ -18,9 +18,31 @@ public class Program {
 
         list.insert("Node4");
         list.display();
-        
+
         System.out.println();
         list.displayTree();
+
+        System.out.println("\n");
+        NodeList2 list2 = new NodeList2("Node1");
+        list2.display();
+
+        list2.insert("Node2");
+        list2.display();
+        
+        list2.insert("Node3");
+        list2.display();   
+        
+        list2.insert("Node4");
+        list2.display();
+
+        list2.remove(2);
+        list2.display();
+
+        try {
+            list2.remove(999);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.printf("Failed to remove node: %s", e.getMessage());
+        }
     }
 }
 
@@ -38,12 +60,23 @@ class OneWayNode {
     // }
 }
 
+class TwoWayNode {
+    String value;
+    TwoWayNode prev;
+    TwoWayNode next;
+
+    TwoWayNode(String value) {
+        this.value = value;
+    }
+}
+
+
 // 1-связный список (head -> node -> tail)
 class NodeList {
     OneWayNode head;
-    
+
     // Конструкторы
-    NodeList () {}    
+    NodeList () {}
 
     NodeList (String value) {
         this.head = new OneWayNode(value);
@@ -54,10 +87,10 @@ class NodeList {
 
         int counter = 1;
         OneWayNode current = this.head;
- 
+
         while (current.next != null) {
-            counter++;
             current = current.next;
+            counter++;
         }
 
         return counter;
@@ -89,7 +122,7 @@ class NodeList {
             this.head = this.head.next;
             return;
         }
-        
+
         int currentIndex = 1;
         OneWayNode prevNode = this.head;
         OneWayNode currentNode = this.head.next;
@@ -99,16 +132,16 @@ class NodeList {
             currentNode = currentNode.next;
             currentIndex++;
         }
-        
+
         prevNode.next = currentNode.next;
-    }    
-    
+    }
+
     public void display() {
         if (length() == 0) {
             System.out.println("Nothing to display. List is empty!");
             return;
         }
-        
+
         OneWayNode current = this.head;
 
         while (current.next != null) {
@@ -129,7 +162,7 @@ class NodeList {
 
         while (current.next != null) {
             System.out.printf("%s(%s)\n", current, current.value);
-            
+
             for (int i = 0; i < offset; i++) { System.out.print("    "); }
             System.out.print("└── ");
 
@@ -138,5 +171,89 @@ class NodeList {
         }
 
         System.out.printf("%s(%s)\n", current, current.value);
+    }
+}
+
+// 2-связный список (head <-> node <-> tail)
+class NodeList2 {
+    TwoWayNode head;
+
+    NodeList2() {}
+
+    NodeList2(String value) {
+        this.head = new TwoWayNode(value);
+    }
+
+    public int length() {
+        if (this.head == null) return 0;
+
+        TwoWayNode current = this.head;
+        int counter = 1;
+        while (current.next != null) {
+            current = current.next;
+            counter++;
+        }
+
+        return counter;
+    }
+
+    public TwoWayNode getTail() {
+        TwoWayNode current = this.head;
+
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        return current;
+    }
+
+    public void insert(String value) {
+        if (this.head == null) {
+            this.head = new TwoWayNode(value);
+            return;
+        }
+
+        TwoWayNode tail = getTail();
+        TwoWayNode node = new TwoWayNode(value);
+
+        tail.next = node;
+        node.prev = tail;
+    }
+
+    public void remove(int index) {
+        if (index >= length() || index < 0) throw new IndexOutOfBoundsException("Index out of range");
+
+        if (index == 0) {
+            this.head = this.head.next;
+            return;
+        }
+
+        int currentIndex = 1;
+        TwoWayNode prevNode = this.head;
+        TwoWayNode currentNode = this.head.next;
+
+        while (currentIndex != index) {
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+
+        prevNode.next = currentNode.next;
+        currentNode.next.prev = prevNode;
+    }
+
+    public void display() {
+        if (length() == 0) {
+            System.out.println("Nothing to display. List is empty!");
+        }
+
+        TwoWayNode current = this.head;
+
+        while (current.next != null) {
+            System.out.printf("%s (%s) <-> ", current, current.value);
+            current = current.next;
+        }
+
+        System.out.printf("%s (%s)\n", current, current.value);
     }
 }

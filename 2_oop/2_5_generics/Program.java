b9078a61dadeae58a5773d47914734c34b9b1310
qsnka <mikhailroot01@gmail.com>
2026-08-04 22:20:17 +0300

    Дополнение к 2.5

--- 2_oop/2_5_generics/Program.java
+++ 2_oop/2_5_generics/Program.java
@@ -6,10 +6,20 @@ public class Program {
         NodeList<Integer> list = new NodeList<Integer>(node1);
         list.show();
 
+        list.remove(1);
+        list.show();
+
+        list.put(6);
+        list.put(9);
+        list.show();
+
         Node<String> node6 = new Node<String>("v4");
         Node<String> node5 = new Node<String>("v3", node6);
         Node<String> node4 = new Node<String>("v1", node5);
         NodeList<String> list2 = new NodeList<String>(node4);
+        list2.show();
+
+        list2.remove(0);
         list2.show();
     }
 }
@@ -29,8 +39,42 @@ class NodeList<T> {
     public NodeList() {}
     public NodeList(Node<T> head) { this.head = head; }
 
-    public void put() {}
-    public void remove() {}
+    private int length() {
+        Node<T> current = this.head;
+        int counter = 0;
+        
+        while (current != null) {
+            counter++;
+            current = current.next;
+        }
+        return counter;
+    }
+    
+    public void put(T value) {
+        Node<T> current = this.head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = new Node<T>(value);
+    }
+    
+    public void remove(int index) {
+        if (index < 0 || index >= length()) throw new IndexOutOfBoundsException("Index out of bounds");
+        if (index == 0) { this.head = this.head.next; return; }
+        
+        Node<T> current = this.head;
+        Node<T> prev = current;
+        int currentIndex = 0;
+
+        while (currentIndex != index) {
+            prev = current;
+            current = current.next;
+            currentIndex++;
+        }
+
+        prev.next = current.next;
+    }
 
     public void show() {
         Node<T> current = this.head;

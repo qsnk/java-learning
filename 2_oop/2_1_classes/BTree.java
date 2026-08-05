08484a80ba9fe192c5ea5f122d0ce819ee6aa8f6
qsnka <mikhailroot01@gmail.com>
2026-08-05 16:15:38 +0300

    Update BTree.java

--- 2_oop/2_1_classes/BTree.java
+++ 2_oop/2_1_classes/BTree.java
@@ -27,13 +27,32 @@ public class BTree {
         lnr(this.root);
     }
 
+    // Прямой обход (NLR)
+    private void nlr(TreeNode node) {
+        if (node == null) return;
+        
+        visit(node);
+        nlr(node.left);
+        nlr(node.right);
+    }
+    
+    // inorder обход (Центрированный обход)
     private void lnr(TreeNode node) {
         if (node == null) return;
         
-        // inorder обход (Центрированный обход (LNR)
         lnr(node.left);
         visit(node);
         lnr(node.right);
+    }
+
+    
+    // Обратный обход (LRN)
+    private void lrn(TreeNode node) {
+        if (node == null) return;
+        
+        lrn(node.left);
+        lrn(node.right);
+        visit(node);
     }
 
     private void visit(TreeNode node) {
@@ -41,17 +60,25 @@ public class BTree {
     }
 
     public static void main(String[] args) {
-        // TreeNode node4 = new TreeNode(1);
-        // TreeNode node3 = new TreeNode(4);
-        // node3.left = node4;
-        // TreeNode node2 = new TreeNode(7);
-        // TreeNode node1 = new TreeNode(5, node3, node2);
+        //     5
+        //    / \
+        //   4   7
+        //  / \
+        // 1   2
 
-        TreeNode node3 = new TreeNode(3);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node1 = new TreeNode(1);
-        node1.right = node2;
-        node2.left = node3;
+        TreeNode node5 = new TreeNode(2);
+        TreeNode node4 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(4);
+        node3.left = node4;
+        node3.right = node5;
+        TreeNode node2 = new TreeNode(7);
+        TreeNode node1 = new TreeNode(5, node3, node2);
+
+        // TreeNode node3 = new TreeNode(3);
+        // TreeNode node2 = new TreeNode(2);
+        // TreeNode node1 = new TreeNode(1);
+        // node1.right = node2;
+        // node2.left = node3;
         
         BTree tree = new BTree(node1);
 
